090975b903dcc547faf22cadc8d99f5f6abc541c
Amina-Delali <aimosta.official@gmail.com>
2025-03-11 06:02:34 +0100

    update build_files

--- build_files.sh
+++ build_files.sh
@@ -1,4 +1,9 @@
-pip install -r requirements.txt
-python3.12 manage.py collectstatic --noinput
-python3.12 manage.py makemigration --noinput
+echo "Building the project..."
+python3.12 -m pip install -r requirements.txt
+
+echo "Make Migration..."
+python3.12 manage.py makemigrations --noinput
 python3.12 manage.py migrate --noinput
+
+echo "Collect Static..."
+python3.12 manage.py collectstatic --noinput --clear
