4df486f5bdb342428ffa90e7956aa3c857ed977a
Amina-Delali <aimosta.official@gmail.com>
2025-03-12 08:58:21 +0100

    my index

--- first/views.py
+++ first/views.py
@@ -2,21 +2,5 @@ from django.shortcuts import render
 
 # Create your views here.
 
-# def index(request):
-#     return render(request, 'first/index.html')
-
-from datetime import datetime
-
-from django.http import HttpResponse
-
 def index(request):
-    now = datetime.now()
-    html = f'''
-    <html>
-        <body>
-            <h1>Hello - this is the basic setup 😱</h1>
-            <p>The current time is { now }---.</p>
-        </body>
-    </html>
-    '''
-    return HttpResponse(html)
+    return render(request, 'first/index.html')
