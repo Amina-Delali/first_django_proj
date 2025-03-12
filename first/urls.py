ed0366518e77a551badb2b025c76ad5668bc39ee
Amina-Delali <aimosta.official@gmail.com>
2025-03-12 08:28:10 +0100

    changed import in views

--- first/urls.py
+++ first/urls.py
@@ -1,7 +1,11 @@
-from django.urls import re_path
-from . import views
+from django.urls import re_path, path
+# from . import views
+from first.views import index
+
 
 urlpatterns = [
-    re_path(r'^$', views.index, name='index'),
+    re_path(r'^$', index, name='index'),
+    #  path('', index),
+
 
 ]
