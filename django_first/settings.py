854317d1c0c530fa75281162896222c533c9868c
Amina-Delali <aimosta.official@gmail.com>
2025-03-12 05:34:35 +0100

    fixing staticfiles issues-user sol

--- django_first/settings.py
+++ django_first/settings.py
@@ -144,18 +144,21 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # STATICFILES_DIRS = (
 # os.path.join(BASE_DIR, 'static'),
 # )
-# STATIC_URL = "static/"
-# STATIC_ROOT = BASE_DIR / "staticfiles" / "static"
-# STATICFILES_DIRS = [BASE_DIR / "static"]
-
 # Static asset configuration
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-# STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles','static'))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles" / "static"
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
+
+
+# STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles','static'))
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles', 'static')
+
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 
