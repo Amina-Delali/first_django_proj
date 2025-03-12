428b1916f09001a946602a91d5e568da35c20e4d
Amina-Delali <aimosta.official@gmail.com>
2025-03-12 08:51:42 +0100

    basics 3

--- first/urls.py
+++ first/urls.py
@@ -1,11 +1,18 @@
-from django.urls import re_path, path
-# from . import views
+# from django.urls import re_path, path
+# # from . import views
+# from first.views import index
+
+
+# urlpatterns = [
+#     # re_path(r'^$', index, name='index'),
+#      path('', index),
+
+# ]
+from django.urls import path
+
 from first.views import index
 
 
 urlpatterns = [
-    # re_path(r'^$', index, name='index'),
-     path('', index),
-
-
+    path('', index),
 ]
--- first/views.py
+++ first/views.py
@@ -5,16 +5,16 @@ from django.shortcuts import render
 # def index(request):
 #     return render(request, 'first/index.html')
 
-from django.http import HttpResponse
 from datetime import datetime
 
+from django.http import HttpResponse
 
 def index(request):
     now = datetime.now()
     html = f'''
     <html>
         <body>
-            <h1>Hello from Vercel!</h1>
+            <h1>Hello - this is the basic setup 😱</h1>
             <p>The current time is { now }.</p>
         </body>
     </html>
